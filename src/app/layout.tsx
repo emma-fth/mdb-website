17269196d56b66c0fd5f434ca4b9a0ba3710ee68
Mike Guerrero <mikxvii@berkeley.edu>
2025-08-02 12:27:22 -0700

    background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-gray-50">
+      <body className="min-h-screen bg-mdb-light-blue">
         <Header />
         
         <main className="flex-grow pt-20">
