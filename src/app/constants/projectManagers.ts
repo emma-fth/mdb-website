5dff820d3174a89b023e8f992389f26722d87971
Emma Feth <efeth012@berkeley.edu>
2026-02-26 12:29:00 -0800

    fixed title typos

--- src/app/constants/projectManagers.ts
+++ src/app/constants/projectManagers.ts
@@ -1,8 +1,8 @@
 import { ProjectManager } from '../types/members'
 
 export const projectManagers: ProjectManager[] = [
-  { name: "Christy Cheung", title: "Member", image: "/project-managers/christy.jpg" },
-  { name: "Chryssa Vamvaka", title: "Member", image: "/project-managers/chryssa.jpeg" },
-  { name: "Henry Bonomolo", title: "Member", image: "/project-managers/henry.jpeg" },
-  { name: "Kaavya Mahendran", title: "Member", image: "/project-managers/kaavya.jpeg" }
+  { name: "Christy Cheung", title: "Project Manager", image: "/project-managers/christy.jpg" },
+  { name: "Chryssa Vamvaka", title: "Project Manager", image: "/project-managers/chryssa.jpeg" },
+  { name: "Henry Bonomolo", title: "Project Manager", image: "/project-managers/henry.jpeg" },
+  { name: "Kaavya Mahendran", title: "Project Manager", image: "/project-managers/kaavya.jpeg" }
 ]
