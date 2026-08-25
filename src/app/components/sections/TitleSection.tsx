b9a88a3df7106366d43c77f6a8d378f56c7e7a01
Arnav Khinvasara <arnavkhinvasara@berkeley.edu>
2026-08-25 14:28:56 -0700

    Use transparent artwork for homepage hero

--- src/app/components/sections/TitleSection.tsx
+++ src/app/components/sections/TitleSection.tsx
@@ -20,8 +20,8 @@ export default function TitleSection() {
             <Image
               src="/images/mdb-home-logo.png"
               alt="MDB @ Berkeley logo"
-              width={2154}
-              height={1678}
+              width={1387}
+              height={1134}
               priority
               sizes="(max-width: 640px) 340px, (max-width: 768px) 400px, (max-width: 1024px) 460px, (max-width: 1280px) 600px, 720px"
               className={`w-[340px] h-[340px] sm:w-[400px] sm:h-[400px] md:w-[460px] md:h-[460px] lg:w-[600px] lg:h-[600px] xl:w-[720px] xl:h-[720px] object-contain transition-all duration-1000 ease-out ${
